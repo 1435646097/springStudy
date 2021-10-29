41a62915cd3305847437ecda1d35e1920ce3f02b
LiaoXM <1435646097@qq.com>
2021-10-29 09:39:00 +0800

    InitializingBean 初始化

--- src/main/java/com/paigu/interview/Decorator/Circle.java
+++ src/main/java/com/paigu/interview/Decorator/Circle.java
@@ -1,14 +1,25 @@
 package com.paigu.interview.Decorator;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
 /**
  * 圆
  *
  * @author LiaoXM
  * @date 2021/10/26
  */
-public class Circle implements IShape {
+@Slf4j
+@Component
+public class Circle implements IShape, InitializingBean {
 	@Override
 	public void draw(){
 		System.out.println("画一个：圆形-----");
+	}
+
+	@Override
+	public void afterPropertiesSet(){
+		log.info("spring boot 初始化完成-----");
 	}
 }
