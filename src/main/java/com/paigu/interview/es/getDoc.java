7acf5c810703e677d5b0deae12f6983d4b2bdfe1
LiaoXM <1435646097@qq.com>
2021-12-06 18:08:30 +0800

    增加es的基础类库和部分代码

---
+++ src/main/java/com/paigu/interview/es/getDoc.java
@@ -0,0 +1,29 @@
+package com.paigu.interview.es;
+
+import com.alibaba.fastjson.JSON;
+import com.paigu.interview.entity.Book;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+public class getDoc {
+	public static void main(String[] args) throws Exception{
+		RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200)));
+		GetRequest request = new GetRequest();
+		request.index("shopping").id("10001");
+//		request.index("shopping").id("10001");
+//		Book book = new Book("小红书", BigDecimal.valueOf(200));
+//		String bookJson = JSON.toJSONString(book);
+//		request.source(bookJson, XContentType.JSON);
+//		client.index(request, RequestOptions.DEFAULT);
+		GetResponse response = client.get(request, RequestOptions.DEFAULT);
+		String sourceAsString = response.getSourceAsString();
+		Book book = JSON.parseObject(sourceAsString, Book.class);
+		System.out.println(sourceAsString);
+//		System.out.println(response.isAcknowledged());
+		client.close();
+	}
+}
