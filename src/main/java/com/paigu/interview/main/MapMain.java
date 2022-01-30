f1acc6ba2b4956d2251745fa93ea719afa018cf8
PaiGu <1435646097@qq.com>
2022-01-30 23:43:17 +0800

    putIfAbsent与computeIfAbsent校验

---
+++ src/main/java/com/paigu/interview/main/MapMain.java
@@ -0,0 +1,23 @@
+package com.paigu.interview.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author PaiGu
+ * @description map测试
+ * @date 2022/1/30 23:34
+ */
+public class MapMain {
+	public static void main(String[] args){
+		HashMap<String, String> map = new HashMap<>(4);
+//		map.put("name", "李四");
+//		String absent = map.putIfAbsent("name", "张三");
+		String absent2 = map.computeIfAbsent("name", key -> "赵丽颖");
+//		System.out.println(absent);
+		System.out.println(absent2);
+		for (Map.Entry<String, String> stringStringEntry : map.entrySet()) {
+			System.out.println(stringStringEntry.getKey() + "-----" + stringStringEntry.getValue());
+		}
+	}
+}
